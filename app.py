936822424a157b7a32cfba8f1e29ca6039b389b2
minsoo <bellamy122@naver.com>
2023-12-13 19:09:33 +0900

    검

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import os
+import urllib
+
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
+
+@app.route('/open', methods=['GET', 'POST'])
+def projectOpen():
+    return render_template('open.html')
+
+@app.route('/dataInsertModal', methods=['GET', 'POST'])
+def dataInsertModal():
+    return render_template('dataInsertModal.html')
 
 
 if __name__ == '__main__':
