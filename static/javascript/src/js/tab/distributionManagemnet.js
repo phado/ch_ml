30572ef25b7362bb9b8ef9262021d16faf0876e1
hyodongE <dlgywjd2822@gmail.com>
2024-01-08 18:31:15 +0900

    0108 pm6 배포관리 상세정보 추가

--- static/javascript/src/js/tab/distributionManagemnet.js
+++ static/javascript/src/js/tab/distributionManagemnet.js
@@ -16,3 +16,9 @@ function detailDistributionModalClose() {
   var detailModal = document.getElementById("distributionDetailModal");
   detailModal.style.display = "none";
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('serviceNameInput').value = "게시판 서비스 이름_끼임";
+  document.getElementById('cctvInput').value = "rtsp://220.125.197.245:10001/kpst";
+});
